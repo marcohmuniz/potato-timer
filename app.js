abaaae0a7c65aa21f378d22e853ab50000a28027
Marco Hernandez-Muniz <marcohmuniz@gmail.com>
2020-12-19 15:40:09 -0500

    Add html structure and css style

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
+const path = require('path')
 const express = require('express')
 const app = express()
 const port = 3000
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.sendFile(path.join(__dirname, 'index.html'))
 })
 
+
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log("Now listening on port 3000")
 })
